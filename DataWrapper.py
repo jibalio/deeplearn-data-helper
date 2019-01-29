c3753d4d72b0d23537811eec7e96cf94e34ea3bc
Student <lerycibalio@gmail.com>
2019-01-29 16:45:27 +0800

    make non-generator version of DataWrapper

--- DataWrapper.py
+++ DataWrapper.py
@@ -19,8 +19,23 @@ class CycleDataWrapper():
             remaining = self.data_size
         else:
             return next(islice(self.generatorX, self.remaining-1, self.remaining)), next(islice(self.generatory, self.remaining-1, self.remaining))
-            
-            
-          
-# usage
-# data_train = DataWrapper(X_train, y_train, 256)
+
+
+class DataWrapper():
+    # does not use an iterator
+    def __init__(self, X_train, y_train, batch_size):
+
+        self._current_instance_idx = 0
+        self.batch_size = 128
+        self.data_size = X_train.shape[0]
+        self.total_batches = self.data_size // batch_size 
+        self.X_train = X_train
+        self.y_train = y_train
+        
+    def fetch_next():
+        batch_X =  X_train[self._current_instance_idx:self._current_instance_idx+self.batch_size]
+        batch_y = y_train[self._current_instance_idx:self._current_instance_idx+self.batch_size]
+        self._current_instance_idx+=self.batch_size
+        if self._current_instance_idx>self.data_size:
+            self._current_instance_idx=0
+        return batch_X, batch_y
